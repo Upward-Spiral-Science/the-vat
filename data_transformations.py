b7d7d24d39c6383141f7cc341a420c89773a2bc2
ttomita <tmtomita87@gmail.com>
2016-03-31 16:39:09 -0400

    modified script for data transformations

--- data_transformations.py
+++ data_transformations.py
@@ -22,13 +22,20 @@ def main():
         X = np.array([[float(e) for e in r] for r in reader])
         Xlog = log_transform(X)
         Xsq = sqrt_transform(X)
-        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_log.npy',Xlog)
-        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_sqrt.npy',Xsq)
+        np.save('/Users/Tyler/synapse_data/synapse_features_log.npy',Xlog)
+        np.save('/Users/Tyler/synapse_data/synapse_features_sqrt.npy',Xsq)
         Xlog = log_transform(X, norm=True)
         Xsq = sqrt_transform(X, norm=True)
-        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_log_normalized.npy',Xlog)
-        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_sqrt_normalized.npy',Xsq)
+        np.save('/Users/Tyler/synapse_data/synapse_features_log_normalized.npy',Xlog)
+        np.save('/Users/Tyler/synapse_data/synapse_features_sqrt_normalized.npy',Xsq)
 
+def main2():
+    with open('/Users/Tyler/synapsinR_7thA.tif.Pivots.txt.2011Features.txt?dl=0','r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        X = np.array([[float(e) for e in r] for r in reader])
+        X = X[:,0:24]
+        np.save('/Users/Tyler/synapse_data/synapse_f0_features.npy',X)
 
 if __name__ == "__main__":
-    main()
+#    main()
+    main2()
