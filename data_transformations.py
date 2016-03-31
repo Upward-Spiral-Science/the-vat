89217e74244092905ea77ba7e04fc25100894c68
ttomita <tmtomita87@gmail.com>
2016-03-31 15:03:08 -0400

    code for data transformations

---
+++ data_transformations.py
@@ -0,0 +1,34 @@
+import numpy as np
+import csv
+
+def normalize(X):
+    mn = np.min(X, axis=0)
+    mx = np.max(X, axis=0)
+    rn = mx - mn
+    X = np.array([(r - mn)/rn for r in X])
+    return X
+
+def log_transform(X, norm=False):
+    X = np.log(normalize(X) + 1) if norm else np.log(X + 1)
+    return X
+
+def sqrt_transform(X, norm=False):
+    X = np.sqrt(normalize(X)) if norm else np.sqrt(X)
+    return X
+
+def main():
+    with open('/Users/Tyler/synapsinR_7thA.tif.Pivots.txt.2011Features.txt?dl=0','r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        X = np.array([[float(e) for e in r] for r in reader])
+        Xlog = log_transform(X)
+        Xsq = sqrt_transform(X)
+        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_log.npy',Xlog)
+        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_sqrt.npy',Xsq)
+        Xlog = log_transform(X, norm=True)
+        Xsq = sqrt_transform(X, norm=True)
+        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_log_normalized.npy',Xlog)
+        np.save('/Users/Tyler/Google Drive/DataScience/synapse_features_sqrt_normalized.npy',Xsq)
+
+
+if __name__ == "__main__":
+    main()
